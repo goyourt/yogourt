e43d508c116c4651870e5789f1f11bcf14c25fd7
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2025-02-22 08:58:19 +0100

    Correctifs init.go, module cli et handler

---
+++ cli/cli.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"CLI_GO/cli/cmd"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+/* --- Fonction Main --- */
+func main() {
+
+	/* Commande root */
+	var rootCmd = &cobra.Command{
+		Use:   "goyourt",
+		Short: "GOyourt CLI",
+		Long:  "Ceci est un CLI pour le package GOyourt.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Bienvenue dans GOyourt !")
+		},
+	}
+
+	rootCmd.AddCommand(cmd.InitCmd)  //Ajout de la commande init dans la commande root
+	rootCmd.AddCommand(cmd.RouteCmd) //Ajout de la commande route dans la commande root
+	rootCmd.AddCommand(cmd.ServeCmd) //Ajout de la commande serve dans la commande root
+	rootCmd.AddCommand(cmd.ModelCmd) //Ajout de la commande model dans la commande root
+
+	if rootCmdError := rootCmd.Execute(); rootCmdError != nil {
+		fmt.Printf("Erreur de chargement du CLI: %v", rootCmdError)
+	}
+}
